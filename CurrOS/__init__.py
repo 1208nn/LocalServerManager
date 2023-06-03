062eabc1d41d9b6d80026eb771299c8c2a3b943f
1208nn <627871794@qq.com>
2023-06-03 19:01:34 +0800

    simplify

--- CurrOS/__init__.py
+++ CurrOS/__init__.py
@@ -1,8 +1,15 @@
 import os
 
+
+import importlib
+
+globals().update(importlib.import_module(f'.{os.name}', __package__).__dict__)
+
+'''
 if os.name == 'nt':
     from . import nt
     globals().update(nt.__dict__)
 else:
     from . import posix
     globals().update(posix.__dict__)
+'''
